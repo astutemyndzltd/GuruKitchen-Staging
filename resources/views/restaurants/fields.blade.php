abcc6856332bf039fdb4f3505cccbeebf107e0a1
Anik Banerjee <anik@astutemyndz.com>
2020-12-30 13:32:10 +0530

    Adding support for preorder v1.7

--- resources/views/restaurants/fields.blade.php
+++ resources/views/restaurants/fields.blade.php
@@ -170,52 +170,50 @@
     </div>
 
     @prepend('scripts')
-    <script type="text/javascript">
-        var var15671147011688676454ble = '';
-        @if(isset($restaurant) && $restaurant->hasMedia('image'))
-        var15671147011688676454ble = {
-            name: "{!! $restaurant->getFirstMedia('image')->name !!}",
-            size: "{!! $restaurant->getFirstMedia('image')->size !!}",
-            type: "{!! $restaurant->getFirstMedia('image')->mime_type !!}",
-            collection_name: "{!! $restaurant->getFirstMedia('image')->collection_name !!}"
-        };
-        @endif
-        var dz_var15671147011688676454ble = $(".dropzone.image").dropzone({
-            url: "{!!url('uploads/store')!!}",
-            addRemoveLinks: true,
-            maxFiles: 1,
-            init: function() {
-                @if(isset($restaurant) && $restaurant->hasMedia('image'))
-                dzInit(this, var15671147011688676454ble, '{!! url($restaurant->getFirstMediaUrl('
-                    image ','
-                    thumb ')) !!}')
-                @endif
-            },
-            accept: function(file, done) {
-                dzAccept(file, done, this.element, "{!!config('medialibrary.icons_folder')!!}");
-            },
-            sending: function(file, xhr, formData) {
-                dzSending(this, file, formData, '{!! csrf_token() !!}');
-            },
-            maxfilesexceeded: function(file) {
-                dz_var15671147011688676454ble[0].mockFile = '';
-                dzMaxfile(this, file);
-            },
-            complete: function(file) {
-                dzComplete(this, file, var15671147011688676454ble, dz_var15671147011688676454ble[0].mockFile);
-                dz_var15671147011688676454ble[0].mockFile = file;
-            },
-            removedfile: function(file) {
-                dzRemoveFile(
-                    file, var15671147011688676454ble, '{!! url("restaurants/remove-media") !!}',
-                    'image', '{!! isset($restaurant) ? $restaurant->id : 0 !!}', '{!! url("uplaods/clear") !!}', '{!! csrf_token() !!}'
-                );
-            }
-        });
-        dz_var15671147011688676454ble[0].mockFile = var15671147011688676454ble;
-        dropzoneFields['image'] = dz_var15671147011688676454ble;
-    </script>
-    @endprepend
+        <script type="text/javascript">
+            var var15671147011688676454ble = '';
+            @if(isset($restaurant) && $restaurant->hasMedia('image'))
+                var15671147011688676454ble = {
+                name: "{!! $restaurant->getFirstMedia('image')->name !!}",
+                size: "{!! $restaurant->getFirstMedia('image')->size !!}",
+                type: "{!! $restaurant->getFirstMedia('image')->mime_type !!}",
+                collection_name: "{!! $restaurant->getFirstMedia('image')->collection_name !!}"
+            };
+                    @endif
+            var dz_var15671147011688676454ble = $(".dropzone.image").dropzone({
+                    url: "{!!url('uploads/store')!!}",
+                    addRemoveLinks: true,
+                    maxFiles: 1,
+                    init: function () {
+                        @if(isset($restaurant) && $restaurant->hasMedia('image'))
+                        dzInit(this, var15671147011688676454ble, '{!! url($restaurant->getFirstMediaUrl('image','thumb')) !!}')
+                        @endif
+                    },
+                    accept: function (file, done) {
+                        dzAccept(file, done, this.element, "{!!config('medialibrary.icons_folder')!!}");
+                    },
+                    sending: function (file, xhr, formData) {
+                        dzSending(this, file, formData, '{!! csrf_token() !!}');
+                    },
+                    maxfilesexceeded: function (file) {
+                        dz_var15671147011688676454ble[0].mockFile = '';
+                        dzMaxfile(this, file);
+                    },
+                    complete: function (file) {
+                        dzComplete(this, file, var15671147011688676454ble, dz_var15671147011688676454ble[0].mockFile);
+                        dz_var15671147011688676454ble[0].mockFile = file;
+                    },
+                    removedfile: function (file) {
+                        dzRemoveFile(
+                            file, var15671147011688676454ble, '{!! url("restaurants/remove-media") !!}',
+                            'image', '{!! isset($restaurant) ? $restaurant->id : 0 !!}', '{!! url("uplaods/clear") !!}', '{!! csrf_token() !!}'
+                        );
+                    }
+                });
+            dz_var15671147011688676454ble[0].mockFile = var15671147011688676454ble;
+            dropzoneFields['image'] = dz_var15671147011688676454ble;
+        </script>
+@endprepend
 
     <!-- Description Field -->
     <div class="form-group row ">
