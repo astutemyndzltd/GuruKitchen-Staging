6eb70d1f05a7185e2ae53910bedb43a130a70e69
Anik Banerjee <anik@astutemyndz.com>
2020-12-31 13:30:18 +0530

    Adding support for preorder v1.9

--- public/js/preorder.js
+++ public/js/preorder.js
@@ -30,8 +30,8 @@ for (let divWeekday of divWeekdays) {
 
             //$(txtStartTime).mdtimepicker();
             //$(txtStartTime).mdtimepicker('setValue', startTime);
-            $(txtEndTime).mdtimepicker();
-            $(txtEndTime).mdtimepicker('setValue', startTime);
+            //$(txtEndTime).mdtimepicker();
+            //$(txtEndTime).mdtimepicker('setValue', startTime);
 
             state[day].push({ opens_at: startTime, closes_at: endTime });
         }
