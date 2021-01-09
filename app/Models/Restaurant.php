d7dd14302f3b69e4b29bf201f380f7f5492be3df
Anik Banerjee <anik@astutemyndz.com>
2021-01-10 01:36:34 +0530

    changed implementation of restaurantcontroller@update

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -113,6 +113,8 @@ class Restaurant extends Model implements HasMedia
         'latitude' => 'required|numeric',
         'admin_commission' => 'required|numeric|min:0',
         'min_order_amount' => 'required|numeric',
+        'phone' => 'required|regex:/(01)[0-9]{9}/',
+        'mobile' => 'required|regex:/(01)[0-9]{9}/'
     ];
 
     /**
@@ -127,6 +129,8 @@ class Restaurant extends Model implements HasMedia
         'longitude' => 'required|numeric',
         'latitude' => 'required|numeric',
         'min_order_amount' => 'required|numeric',
+        'phone' => 'required|regex:/(01)[0-9]{9}/',
+        'mobile' => 'required|regex:/(01)[0-9]{9}/'
     ];
 
     /**
