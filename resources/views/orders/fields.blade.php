2134160748c5618c8a6b8998e08f8dbceed4047f
Anik Banerjee <anik@astutemyndz.com>
2021-01-10 11:53:20 +0530

    Added opening hours validation rule 1.0

--- resources/views/orders/fields.blade.php
+++ resources/views/orders/fields.blade.php
@@ -6,14 +6,14 @@
     <input type="hidden" value="{{ json_encode($order) }}" id="anik">
 
     <!-- User Id Field -->
-    <div class="form-group row ">
+    {{-- <div class="form-group row ">
         {!! Form::label('user_id', trans("lang.order_user_id"),['class' => 'col-3 control-label text-right']) !!}
         <div class="col-9">
             <input type="hidden" name="user_id" id="hdnUserId">
             {!! Form::select('user_id', $user, null, ['class' => 'select2 form-control']) !!}
             <div class="form-text text-muted">{{ trans("lang.order_user_id_help") }}</div>
         </div>
-    </div>
+    </div> --}}
 
     <!-- Driver Id Field -->
     <div class="form-group row ">
@@ -33,8 +33,8 @@
         </div>
     </div>
 
-    <!-- Status Field -->
-    <div class="form-group row ">
+    <!-- Payment Status Field -->
+    {{-- <div class="form-group row ">
         {!! Form::label('status', trans("lang.payment_status"),['class' => 'col-3 control-label text-right']) !!}
         <div class="col-9">
             {!! Form::select('status',
@@ -46,7 +46,8 @@
             , isset($order->payment) ? $order->payment->status : '', ['class' => 'select2 form-control']) !!}
             <div class="form-text text-muted">{{ trans("lang.payment_status_help") }}</div>
         </div>
-    </div>
+    </div> --}}
+
     <!-- 'Boolean active Field' -->
     <div class="form-group row ">
         {!! Form::label('active', trans("lang.order_active"),['class' => 'col-3 control-label text-right']) !!}
@@ -62,7 +63,7 @@
 <div style="flex: 50%;max-width: 50%;padding: 0 4px;" class="column">
 
     <!-- Tax Field -->
-    <div class="form-group row ">
+    {{-- <div class="form-group row ">
         {!! Form::label('tax', trans("lang.order_tax"), ['class' => 'col-3 control-label text-right']) !!}
         <div class="col-9">
             {!! Form::number('tax', null,  ['class' => 'form-control', 'step'=>"any",'placeholder'=>  trans("lang.order_tax_placeholder")]) !!}
@@ -70,10 +71,10 @@
                 {{ trans("lang.order_tax_help") }}
             </div>
         </div>
-    </div>
+    </div> --}}
 
     <!-- delivery_fee Field -->
-    <div class="form-group row ">
+    {{-- <div class="form-group row ">
         {!! Form::label('delivery_fee', trans("lang.order_delivery_fee"), ['class' => 'col-3 control-label text-right']) !!}
         <div class="col-9">
             {!! Form::number('delivery_fee', null,  ['class' => 'form-control','step'=>"any",'placeholder'=>  trans("lang.order_delivery_fee_placeholder")]) !!}
@@ -81,7 +82,8 @@
                 {{ trans("lang.order_delivery_fee_help") }}
             </div>
         </div>
-    </div>
+    </div> --}}
+
     <!-- Hint Field -->
     <div class="form-group row ">
         {!! Form::label('hint', trans("lang.order_hint"), ['class' => 'col-3 control-label text-right']) !!}
@@ -91,6 +93,7 @@
             <div class="form-text text-muted">{{ trans("lang.order_hint_help") }}</div>
         </div>
     </div>
+    
 </div>
 @if($customFields)
     <div class="clearfix"></div>
