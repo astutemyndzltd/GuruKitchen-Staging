0b4d43a3eccceccc85b14d468a883815a1a9e6d9
Anik Banerjee <anik@astutemyndz.com>
2021-01-16 00:51:36 +0530

    Support for priority index v1.0

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -45,6 +45,9 @@
         <li class="nav-item">
           <a class="nav-link active" href="{!! url()->current() !!}"><i class="fa fa-plus mr-2"></i>{{trans('lang.category_create')}}</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{!! route('categories.sort') !!}"><i class="fa fa-sort-numeric-asc mr-2"></i>Rearrange Categories</a>
+        </li>
       </ul>
     </div>
     <div class="card-body">
