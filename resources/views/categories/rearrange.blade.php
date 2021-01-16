7e18653a5cb210c6bf201122526748bd820e2ab8
Anik Banerjee <anik@astutemyndz.com>
2021-01-16 15:35:46 +0530

    Priority Index Support v1.0

--- resources/views/categories/rearrange.blade.php
+++ resources/views/categories/rearrange.blade.php
@@ -73,7 +73,7 @@
           </ul>
         </div>
         <div class="form-group col-12 text-right">
-          <button type="submit" class="btn btn-{{setting('theme_color')}}" ><i class="fa fa-save"></i> {{trans('lang.save')}} {{trans('lang.category')}}</button>
+          <button type="submit" class="btn btn-{{setting('theme_color')}}" ><i class="fa fa-save"></i> {{trans('lang.save')}} Arrangement</button>
           <a href="{!! route('categories.index') !!}" class="btn btn-default"><i class="fa fa-undo"></i> {{trans('lang.cancel')}}</a>
         </div>
       </form>
