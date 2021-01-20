d1792cd2fbbe33f24b0aa1f3ea6ced52a1425c74
Anik Banerjee <anik@astutemyndz.com>
2021-01-21 04:18:54 +0530

    Stripe Integration v1.0

---
+++ app/Models/FoodOrderExtra.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * File name: FoodOrder.php
+ * Last modified: 2020.06.11 at 16:10:52
+ * Author: SmarterVision - https://codecanyon.net/user/smartervision
+ * Copyright (c) 2020
+ */
+
+namespace App\Models;
+
+use Eloquent as Model;
+
+class FoodOrderExtra extends Model
+{
+
+    public $table = 'food_order_extras';
+    
+
+    public $fillable = [
+        'food_order_id',
+        'extra_id',
+        'price'
+    ];
+
+    protected $casts = [
+        'food_order_id' => 'integer',
+        'extra_id' => 'integer',
+        'price' => 'double'
+    ];
+
+   
+    public function foodOrder() 
+    {
+        $this->belongsTo(\App\Models\FoodOrder::class, 'food_order_id', 'id');
+    }
+    
+    public function extra() 
+    {
+        $this->belongsTo(\App\Models\Extra::class, 'extra_id', 'id');
+    }
+}
