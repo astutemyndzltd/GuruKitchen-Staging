8279185e804a701fa8914480d7864d668bddae6e
Anik Banerjee <anik@astutemyndz.com>
2021-02-15 03:41:52 +0530

    Fix delivery address update v1.0

--- app/Http/Controllers/API/DeliveryAddressAPIController.php
+++ app/Http/Controllers/API/DeliveryAddressAPIController.php
@@ -78,8 +78,13 @@ class DeliveryAddressAPIController extends Controller
      */
     public function store(Request $request)
     {
+        $input = $request->all();
+        file_put_contents('order.txt', json_encode($input));
+        
         $uniqueInput = $request->only("address");
         $otherInput = $request->except("address");
+
+
         try {
             $deliveryAddress = $this->deliveryAddressRepository->updateOrCreate($uniqueInput, $otherInput);
 
