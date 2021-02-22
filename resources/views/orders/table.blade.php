e2ef215a8563a1170be83526cffd2ed206b8da23
Anik Banerjee <anik@astutemyndz.com>
2021-02-23 01:25:21 +0530

    Order datatable customization

--- resources/views/orders/table.blade.php
+++ resources/views/orders/table.blade.php
@@ -12,8 +12,14 @@
  $(window).on('load', () => {
 
     const table = $('#dataTableBuilder').DataTable();
+
     let $divForCheckbox = $('<div class="col-lg-4 col-xs-12"></div>');
     $divForCheckbox.insertAfter('#dataTableBuilder_wrapper>div.row>div');
+
+    const $checkbox = $('<input type="checkbox">');
+    $checkbox.appendTo($divForCheckbox);
+
+    $($checkbox).iCheck();
 
  });
 </script>
