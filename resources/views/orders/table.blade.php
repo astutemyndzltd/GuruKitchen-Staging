f62d43fa272b382af85610d3f7306e4d88131454
Anik Banerjee <anik@astutemyndz.com>
2021-02-22 23:50:10 +0530

    Support for plain text in summernote v1

--- resources/views/orders/table.blade.php
+++ resources/views/orders/table.blade.php
@@ -9,7 +9,23 @@
 {!! $dataTable->scripts() !!}
 <script>
  $(window).on('load', () => {
+
     $('#dataTableBuilder thead tr').clone(true).appendTo('#dataTableBuilder thead');
+
+    $('#dataTableBuilder thead tr:eq(1) th').each( function (i) {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+ 
+        $( 'input', this ).on( 'keyup change', function () {
+            if ( table.column(i).search() !== this.value ) {
+                table
+                    .column(i)
+                    .search( this.value )
+                    .draw();
+            }
+        });
+    });
+    
     //let dataTable = $('#dataTableBuilder').DataTable();
     //console.log(dataTable);
  });
