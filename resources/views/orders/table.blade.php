aa8468e3fc347ca5da1c32497b5ea3dec7f85105
Anik Banerjee <anik@astutemyndz.com>
2021-02-23 01:19:35 +0530

    Order datatable customization

--- resources/views/orders/table.blade.php
+++ resources/views/orders/table.blade.php
@@ -13,33 +13,7 @@
 
     const table = $('#dataTableBuilder').DataTable();
     let $divForCheckbox = $('<div class"col-lg-4 col-xs-12"></div>');
-    $('#dataTableBuilder_wrapper div.row:eq(1)>div').append($divForCheckbox);
-    
-
-    /*$('#dataTableBuilder thead tr').clone(true).appendTo('#dataTableBuilder thead');
-
-    $('#dataTableBuilder thead tr:eq(1) th').each(function (i) {
-
-        let title = $(this).text();
-
-        if (title == 'Actions') {
-            $(this).html('');
-        }
-
-        $(this).attr('class', 'search-cell-header');
-
-        $(this).html( '<input type="text" class="search-cell" placeholder="Search" />' );
- 
-        $('input', this).on('keyup change', function () {
-            if (table.column(i).search() !== this.value) {
-                table.column(i).search(this.value).draw();
-            }
-        });
-
-    });*/
-
-    //let dataTable = $('#dataTableBuilder').DataTable();
-    //console.log(dataTable);
+    $divForCheckbox.insertAfter('#dataTableBuilder_wrapper div.row:eq(1)>div');
 
 
  });
