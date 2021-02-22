2c312a0802e78610dab4b1dad32971ced51bf81a
Anik Banerjee <anik@astutemyndz.com>
2021-02-23 01:14:17 +0530

    Order datatable customization

--- resources/views/orders/table.blade.php
+++ resources/views/orders/table.blade.php
@@ -12,34 +12,9 @@
  $(window).on('load', () => {
 
     const table = $('#dataTableBuilder').DataTable();
-
-    $('#dataTableBuilder_wrapper').append('<div>Anik</div>');
+    let $divForCheckbox = $('<div class"col-lg-4 col-xs-12"></div>');
+    $('#dataTableBuilder_wrapper div.row:eq(1)>div').append($divForCheckbox);
     
-
-    //$('#dataTableBuilder thead tr').clone(true).appendTo('#dataTableBuilder thead');
-
-    /*$('#dataTableBuilder thead tr:eq(1) th').each(function (i) {
-
-        let title = $(this).text();
-
-        if (title == 'Actions') {
-            $(this).html('');
-        }
-
-        $(this).attr('class', 'search-cell-header');
-
-        $(this).html( '<input type="text" class="search-cell" placeholder="Search" />' );
- 
-        $('input', this).on('keyup change', function () {
-            if (table.column(i).search() !== this.value) {
-                table.column(i).search(this.value).draw();
-            }
-        });
-
-    });*/
-
-    //let dataTable = $('#dataTableBuilder').DataTable();
-    //console.log(dataTable);
 
 
  });
