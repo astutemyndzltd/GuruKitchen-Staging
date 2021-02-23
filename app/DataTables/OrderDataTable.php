21304466a59c3990673b0df88219a2008aea92e5
Anik Banerjee <anik@astutemyndz.com>
2021-02-23 23:10:10 +0530

    live order view support

--- app/DataTables/OrderDataTable.php
+++ app/DataTables/OrderDataTable.php
@@ -205,7 +205,7 @@ class OrderDataTable extends DataTable
     {
         return $this->builder()
             ->columns($this->getColumns())
-            ->ajax(['data' => 'function(d) { d.key = "value"; }'])
+            ->ajax(['data' => 'function(d) { onReloadDt(d); }'])
             ->addAction(['title'=>trans('lang.actions'),'width' => '80px', 'printable' => false, 'responsivePriority' => '100'])
             ->parameters(array_merge(
                 [
