925cfad1ffd86367896c9b5470a9810c060d09f7
Anik Banerjee <anik@astutemyndz.com>
2021-02-23 20:40:46 +0530

    live order filter support

--- resources/views/orders/table.blade.php
+++ resources/views/orders/table.blade.php
@@ -21,13 +21,12 @@
     const $checkbox = $(html).appendTo($divForCheckbox);
     $checkbox.iCheck({ checkboxClass: 'icheckbox_flat-blue' });
 
-    $checkbox.on('ifChecked', () => { showLiveOrders = true; $table.ajax.reload() });
-    $checkbox.on('ifUnchecked', () => { showLiveOrders = false; $tabe.ajax.reload() });
+    $checkbox.on('ifChecked', () => { showLiveOrders = true; $table.ajax.reload(); });
+    $checkbox.on('ifUnchecked', () => { showLiveOrders = false; $tabe.ajax.reload(); });
 
     $table.DataTable({ajax: { data: (d) => d.showLive = showLiveOrders }});
 
     window.dt = $table;
-
 
 
  });
