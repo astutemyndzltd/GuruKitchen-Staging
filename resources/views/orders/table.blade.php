9ea98a40cad6ad1a974aac55d4cf77ccb83bb9e7
Anik Banerjee <anik@astutemyndz.com>
2021-02-23 11:29:54 +0530

    live order filter support

--- resources/views/orders/table.blade.php
+++ resources/views/orders/table.blade.php
@@ -11,24 +11,14 @@
 <script>
  $(window).on('load', () => {
 
-    console.log('my name is anik banerjee');
-
     const table = $('#dataTableBuilder').DataTable();
 
     let $divForCheckbox = $('<div class="col-lg-4 col-xs-12"></div>');
     $divForCheckbox.insertBefore('#dataTableBuilder_wrapper div.ml-auto');
 
-
-    let html = `<input type="checkbox">
-                <label>Show Live Orders</label>`;
-
-
-    const $checkbox = $(html);
-    $checkbox.appendTo($divForCheckbox);
-    $checkbox.iCheck({
-    checkboxClass: 'icheckbox_flat-blue',
-    //radioClass: 'iradio_flat-red'
-     });
+    let html = `<label><input type="checkbox">Show Live Orders</label>`;
+    const $checkbox = $(html).appendTo($divForCheckbox);
+    $checkbox.iCheck({ checkboxClass: 'icheckbox_flat-blue' });
 
  });
 </script>
