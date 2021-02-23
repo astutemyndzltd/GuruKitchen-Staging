a6a0c4bd67b5b786a147cc84295c3eccb6a1aa01
Anik Banerjee <anik@astutemyndz.com>
2021-02-23 20:44:46 +0530

    live order filter support

--- resources/views/orders/table.blade.php
+++ resources/views/orders/table.blade.php
@@ -11,7 +11,7 @@
 <script>
  $(window).on('load', () => {
 
-    const $table = $('#dataTableBuilder').DataTable({ajax: { data: (d) => d.showLive = showLiveOrders }});
+    const $table = $('#dataTableBuilder').DataTable({ajax: { data: (d) => console.log(d) }});
     let showLiveOrders = true;
 
     let $divForCheckbox = $('<div class="col-lg-4 col-xs-12 live-order"></div>');
