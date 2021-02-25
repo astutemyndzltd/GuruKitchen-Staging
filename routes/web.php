fdd371511d160a013ab37e21930332cb67d70eba
Anik Banerjee <anik@astutemyndz.com>
2021-02-25 21:16:08 +0530

    live order view support

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@
 Route::get('categories/rearrange', 'CategoryController@rearrange');
 Route::post('categories/store-rearranged', 'CategoryController@storeRearranged');
 
-Route::get('sales', 'SalesController@index');
+Route::resource('sales', 'OrderController');
 
 Route::get('login/{service}', 'Auth\LoginController@redirectToProvider');
 Route::get('login/{service}/callback', 'Auth\LoginController@handleProviderCallback');
