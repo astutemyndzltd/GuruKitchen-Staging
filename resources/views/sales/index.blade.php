479535cac5de7324a8f04cd2665562c5a3f3e31f
Anik Banerjee <anik@astutemyndz.com>
2021-02-26 00:38:43 +0530

    live order view support

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -6,14 +6,13 @@
         <div class="row mb-2">
 
             <div class="col-sm-6">
-                <h1 class="m-0 text-dark">Sales<small class="ml-3 mr-3">
+                <h1 class="m-0 text-dark"> Sales<small class="ml-3 mr-3"></small></h1>
             </div>
 
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="{{url('/dashboard')}}"><i class="fa fa-dashboard"></i> {{trans('lang.dashboard')}}</a></li>
-                    <li class="breadcrumb-item"><a href="{!! route('orders.index') !!}">{{trans('lang.order_plural')}}</a>
-                    </li>
+                    <li class="breadcrumb-item"><a href="{!! route('orders.index') !!}">{{trans('lang.order_plural')}}</a></li>
                     <li class="breadcrumb-item active">{{trans('lang.order_table')}}</li>
                 </ol>
             </div>
