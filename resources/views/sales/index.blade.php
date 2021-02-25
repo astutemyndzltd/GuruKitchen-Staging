4d3aca9ed197ab68c417c223aa52143dfa311ccb
Anik Banerjee <anik@astutemyndz.com>
2021-02-26 00:35:53 +0530

    live order view support

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -2,13 +2,24 @@
 
 @section('content')
 <div class="content-header">
-  <div class="container-fluid">
-    <div class="row mb-2">
-        <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Sales<small class="ml-3 mr-3">|</small><small>{{trans('lang.order_desc')}}</small></h1>
+    <div class="container-fluid">
+        <div class="row mb-2">
+
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Sales<small class="ml-3 mr-3">
+            </div>
+
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{url('/dashboard')}}"><i class="fa fa-dashboard"></i> {{trans('lang.dashboard')}}</a></li>
+                    <li class="breadcrumb-item"><a href="{!! route('orders.index') !!}">{{trans('lang.order_plural')}}</a>
+                    </li>
+                    <li class="breadcrumb-item active">{{trans('lang.order_table')}}</li>
+                </ol>
+            </div>
+
         </div>
     </div>
-  </div>
 </div>
 
 @endsection
