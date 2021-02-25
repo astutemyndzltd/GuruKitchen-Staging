ff71ec261edcd49cdcad0386454c7a23a2aa6031
Anik Banerjee <anik@astutemyndz.com>
2021-02-25 21:35:03 +0530

    live order view support

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -144,7 +144,7 @@
 @endcan
 
 <li class="nav-item">
-    <a class="nav-link {{ Request::is('sales*') ? 'active' : '' }}" href="/sales">
+    <a class="nav-link {{ Request::is('sales*') ? 'active' : '' }}" href="{!! route('sales.index') !!}">
         <i class="nav-icon fa fa-file-text"></i><p>Sales</p>
     </a>
 </li>
