8402ef8743bf335db84ccba8c2b89b935381ed03
Anik Banerjee <anik@astutemyndz.com>
2021-02-26 23:42:04 +0530

    m3

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -57,7 +57,7 @@
 
 @push('css_lib')
     <link rel="stylesheet" href="{{ asset('css/sales-datatable.css') }}">
-    <link rel="stylesheet" href="{{ asset('plugins/daterangepicker-bs3.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker-bs3.css') }}">
 @endpush
 
 @push('scripts_lib')
