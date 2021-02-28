9f21650732dfd3fcdc338f2cae9d300579ae4996
Anik Banerjee <anik@astutemyndz.com>
2021-02-28 17:37:12 +0530

    m3

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -64,34 +64,4 @@
 @push('scripts_lib')
 <script type="text/javascript" src="{{ asset('plugins/daterangepicker/moment.js') }}"></script>
 <script type="text/javascript" src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
-<script>
-    $(window).on('load', () => {
-
-        let start = moment();
-        let end = moment();
-
-        function cb(start, end) {
-            //$('#daterangepicker span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-            //console.log('hello world');
-        }
-
-        $('#daterangepicker').daterangepicker({
-            startDate: start,
-            endDate: end,
-            locale: {
-                format: 'DD.MM.YYYY'
-            },
-            ranges: {
-                'Today': [moment(), moment()],
-                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                'This Month': [moment().startOf('month'), moment().endOf('month')],
-                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-            }
-        }, cb);
-
-        cb(start, end);
-    });
-</script>
 @endpush
--- resources/views/sales/table.blade.php
+++ resources/views/sales/table.blade.php
@@ -6,5 +6,41 @@
 
 @push('scripts_lib')
 @include('layouts.datatables_js')
+<script>
+    let start = moment();
+    let end = moment();
+
+    function onReloadDt(data) {
+        console.log(start.)
+    }
+
+</script>
 {!! $dataTable->scripts() !!}
+<script>
+    $(window).on('load', () => {
+
+        const $table = $('#dataTableBuilder').DataTable();
+
+        $('#daterangepicker').daterangepicker({
+            startDate: start,
+            endDate: end,
+            locale: { format: 'DD MMM YYYY' },
+            ranges: {
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            }
+        }, onDateRangeChange);
+
+        function onDateRangeChange(s, e) {
+            start = s; end = e;
+            $table.ajax.reload();
+        }
+
+        onDateRangeChange(start, end);
+    });
+</script>
 @endpush
