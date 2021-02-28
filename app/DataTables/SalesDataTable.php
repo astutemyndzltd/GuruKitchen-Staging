a73738f8679ad7e5911bae09eea21de4974844f0
Anik Banerjee <anik@astutemyndz.com>
2021-02-28 22:16:01 +0530

    m3

--- app/DataTables/SalesDataTable.php
+++ app/DataTables/SalesDataTable.php
@@ -21,7 +21,7 @@ class SalesDataTable extends DataTable
     {
        
         $dataTable = new EloquentDataTable($query);
-        file_put_contents('order.txt', json_encode($dataTable));
+        file_put_contents('order.txt', json_encode($query));
         $columns = array_column($this->getColumns(), 'data');
         $dataTable = $dataTable
                     ->editColumn('id', function ($order) {
