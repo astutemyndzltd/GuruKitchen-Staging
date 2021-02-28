a5342fe6f0d290fcd02a17d6609d154c612fd39e
Anik Banerjee <anik@astutemyndz.com>
2021-02-28 21:56:15 +0530

    m3

--- app/DataTables/SalesDataTable.php
+++ app/DataTables/SalesDataTable.php
@@ -82,7 +82,7 @@ class SalesDataTable extends DataTable
                 ->join("user_restaurants", "user_restaurants.restaurant_id", "=", "foods.restaurant_id")
                 ->where('user_restaurants.user_id', auth()->id());
 
-            $model = $model->whereRaw('orders.order_status_id = 5')->whereRaw("date(orders.created_at) between '$start' and '$end'");    
+            $model = $model->whereRaw('orders.order_status_id = 5') 
             $model = $model->groupBy('orders.id')->select('orders.*');
 
             return $model;
