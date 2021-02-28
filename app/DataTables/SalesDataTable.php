e7cbf65173000af4eba82bdb188f662bbc852ee6
Anik Banerjee <anik@astutemyndz.com>
2021-03-01 00:08:23 +0530

    Support for earning

--- app/DataTables/SalesDataTable.php
+++ app/DataTables/SalesDataTable.php
@@ -36,7 +36,7 @@ class SalesDataTable extends DataTable
                         $price = $order->payment->price;
                         $adminCommission = $order->foods[0]->restaurant->admin_commission;
                         $commission = $price * $adminCommission / 100;
-                        $grossCommission = $commission + ($commission * Config::get('services.default_tax') / 100);                        
+                        $grossCommission = $commission + ($commission * setting('default_tax', 0) / 100);                        
                         return $grossCommission;
                     })    
                     ->addColumn('action', 'sales.datatables_actions')
