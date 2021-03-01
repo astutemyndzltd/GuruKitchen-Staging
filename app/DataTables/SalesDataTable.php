059fcf8feddcd13f8ad9423c3f02010bffec84a9
Anik Banerjee <anik@astutemyndz.com>
2021-03-01 12:19:23 +0530

    Support for earning

--- app/DataTables/SalesDataTable.php
+++ app/DataTables/SalesDataTable.php
@@ -87,7 +87,7 @@ class SalesDataTable extends DataTable
     {
         return $this->builder()
             ->columns($this->getColumns())
-            ->ajax(['data' => 'function(d) { onReloadDt(d); }', 'dataSrc' => 'function(d) { onDataReceived(d); }'])
+            ->ajax(['data' => 'function(d) { onReloadDt(d); }' ])
             ->addAction(['title'=>trans('lang.actions'),'width' => '80px', 'printable' => false, 'responsivePriority' => '100'])
             ->parameters(array_merge(
                 [
