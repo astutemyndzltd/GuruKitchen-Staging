2aedf9e9334ef57766e7cd46dfee4a7425b31f38
Anik Banerjee <anik@astutemyndz.com>
2021-03-03 02:14:04 +0530

    m3

--- public/js/payout.js
+++ public/js/payout.js
@@ -20,10 +20,8 @@ function onDateRangeChange(start, end) {
 
 const daterangepicker = $(txtPayoutPeriod).daterangepicker(datePickerOptions, onDateRangeChange);
 
-$('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+$(txtPayoutPeriod).on('apply.daterangepicker', (ev, picker) => {
     $(this).val(picker.startDate.format('DD MMM YYYY') + ' - ' + picker.endDate.format('DD MMM YYYY'));
 });
 
-$('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
-    $(this).val('');
-});
+$(txtPayoutPeriod).on('cancel.daterangepicker', (ev, picker) => $(this).val(''));
