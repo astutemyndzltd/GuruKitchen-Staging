670c0eb3c1293da3b2de8c32a937ac24decaaf2b
Anik Banerjee <anik@astutemyndz.com>
2021-03-02 17:18:54 +0530

    Support for earning

--- resources/views/orders/show_fields.blade.php
+++ resources/views/orders/show_fields.blade.php
@@ -139,6 +139,16 @@
 
 @endif
 
+@if(auth()->user()->hasRole('admin') && isset($order->payment->transaction_id))
+<!-- Transaction ID -->
+<div class="form-group row col-md-8 col-sm-12">
+  {!! Form::label('transaction_id', 'Transaction ID, ['class' => 'col-2 control-label']) !!}
+  <div class="col-9">
+    <p style="text-align:justify;">Expected by {!! $order->payment->transaction_id !!}</p>
+  </div>
+</div>
+@endif
+
 
 {{--<!-- Tax Field -->--}}
 {{--<div class="form-group row col-md-6 col-sm-12">--}}
