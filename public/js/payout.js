c7b0b0b85aa927faa70acc5bebbff6c6f25736d0
Anik Banerjee <anik@astutemyndz.com>
2021-03-03 02:45:09 +0530

    m3

--- public/js/payout.js
+++ public/js/payout.js
@@ -6,9 +6,10 @@ const datePickerOptions = { locale: {  format: 'DD MMM YYYY' } };
 
 function onDateRangeChange(start, end) {
     let restaurantId = $(ddlRestaurants).val();
-    console.log(restaurantId);
     let startDate = start.format('YYYY-MM-DD');
-    let endDate = end.format('YYYY-MM-DD');    
+    let endDate = end.format('YYYY-MM-DD');
+    
+    
 }
 
 const daterangepicker = $(txtPayoutPeriod).daterangepicker(datePickerOptions, onDateRangeChange);
