70593b99c64f933eef580be061cb57532bd27cee
Anik Banerjee <anik@astutemyndz.com>
2021-03-03 02:28:44 +0530

    m3

--- public/js/payout.js
+++ public/js/payout.js
@@ -2,17 +2,12 @@ const txtPayoutPeriod = document.querySelector('#txtPayoutPeriod');
 const ddlRestaurants = document.querySelector('#restaurant_id');
 const txtAmount = document.querySelector('#amount');
 
-let datePickerOptions = {
-    locale: { 
-        format: 'DD MMM YYYY'
-    }
-};
-
+let datePickerOptions = { locale: {  format: 'DD MMM YYYY' } };
 
 function onDateRangeChange(start, end) {
-
+    
 }
-
 
 const daterangepicker = $(txtPayoutPeriod).daterangepicker(datePickerOptions, onDateRangeChange);
 txtPayoutPeriod.value = '';
+
