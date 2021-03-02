c093e02f352e6b583f9aae76d98cc0c4f49ca0d5
Anik Banerjee <anik@astutemyndz.com>
2021-03-03 02:20:09 +0530

    m3

--- public/js/payout.js
+++ public/js/payout.js
@@ -3,13 +3,9 @@ const ddlRestaurants = document.querySelector('#restaurant_id');
 const txtAmount = document.querySelector('#amount');
 
 let datePickerOptions = {
-
     locale: { 
-        format: 'DD MMM YYYY', 
-        cancelLabel: 'Clear' 
-    },
-
-    autoUpdateInput: false 
+        format: 'DD MMM YYYY'
+    }
 };
 
 
@@ -19,9 +15,3 @@ function onDateRangeChange(start, end) {
 
 
 const daterangepicker = $(txtPayoutPeriod).daterangepicker(datePickerOptions, onDateRangeChange);
-
-$(txtPayoutPeriod).on('apply.daterangepicker', (ev, picker) => {
-    $(this).val(picker.startDate.format('DD MMM YYYY') + ' - ' + picker.endDate.format('DD MMM YYYY'));
-});
-
-$(txtPayoutPeriod).on('cancel.daterangepicker', (ev, picker) => $(this).val(''));
