dbf2ccc17bba991c97ea233e3807d7eac7938c0d
Anik Banerjee <anik@astutemyndz.com>
2021-03-04 14:38:21 +0530

    Payout Support

--- resources/views/payout_history/index.blade.php
+++ resources/views/payout_history/index.blade.php
@@ -6,7 +6,7 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1 class="m-0 text-dark">{{trans('lang.restaurants_payout_plural')}}</h1>
+        <h1 class="m-0 text-dark">Payout History</h1>
       </div><!-- /.col -->
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
