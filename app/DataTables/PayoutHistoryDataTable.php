ac1921623e6412b7709f7817cf36814857f6eaa3
Anik Banerjee <anik@astutemyndz.com>
2021-03-05 02:58:45 +0530

    Payout Support

--- app/DataTables/PayoutHistoryDataTable.php
+++ app/DataTables/PayoutHistoryDataTable.php
@@ -32,7 +32,7 @@ class PayoutHistoryDataTable extends DataTable
                 return getPriceColumn($payout, 'amount');
             })
             ->editColumn('created_at', function ($payout) {
-                return getPriceColumn($payout, 'amount');
+                return getDateColumn($payout, 'created_at');
             })
             ->rawColumns(array_merge($columns));
 
