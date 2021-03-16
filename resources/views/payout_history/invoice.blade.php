c23f8cc747c5b5ff21e68a4cd3beeb4980a12c75
Anik Banerjee <anik@astutemyndz.com>
2021-03-16 21:36:48 +0530

    Invoice Support

--- resources/views/payout_history/invoice.blade.php
+++ resources/views/payout_history/invoice.blade.php
@@ -1,21 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
   <head>
     <meta charset="utf-8">
     <title>Invoice</title>
     <style>
       
+      @font-face {
+        font-family: SourceSansPro;
+        src: url(SourceSansPro-Regular.ttf);
+      }
+
       .clearfix:after {
         content: "";
         display: table;
         clear: both;
       }
-      
+
       a {
         color: #0087C3;
         text-decoration: none;
       }
-      
+
       body {
         position: relative;
         width: 21cm;  
@@ -27,95 +33,97 @@
         font-size: 14px; 
         font-family: SourceSansPro;
       }
-      
-      header {
+
+      div#header {
         padding: 10px 0;
         margin-bottom: 20px;
         border-bottom: 1px solid #AAAAAA;
       }
-      
+
       #logo {
         float: left;
         margin-top: 18px;
       }
-      
+
       #logo img {
         height: 70px;
       }
-      
-      main {
+
+      #main {
         margin-top: 35px;
       }
-      
-      
+
+
       #company {
         float: right;
         text-align: right;
       }
-      
-      
+
+
       #details {
         margin-bottom: 50px;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
+        display: table;
+        width: 100%;
       }
-      
+
       #client {
         padding-left: 6px;
         border-left: 6px solid #0087C3;
         float: left;
+        display: table-row;
       }
-      
+
       #client .to {
         color: #777777;
       }
-      
+
       h2.name {
         font-size: 1.4em;
         margin: 0;
         margin-top: 3px;
+        font-weight: 700;
       }
-      
+
       div.address {
         width: 300px;
         margin-top: 3px;
       }
-      
+
       div.phone {
         width: 300px;
         margin-top: 3px;
       }
-      
+
       #company > div {
         margin-top: 3px;
       }
-      
+
       #invoice {
         float: right;
         text-align: right;
+        display: table-row;
       }
-      
+
       #invoice h1 {
         color: #0087C3;
         font-size: 2.4em;
         line-height: 1em;
         margin: 0  0 10px 0;
       }
-      
+
       #invoice .date {
         font-size: 1.1em;
         color: #777777;
         margin-bottom: 4px;
       }
-      
+
       table {
         width: 100%;
         border-collapse: collapse;
         border-spacing: 0;
         margin-bottom: 20px;
       }
-      
+
       table th,
       table td {
         padding: 20px;
@@ -123,48 +131,53 @@
         text-align: left;
         border-bottom: 1px solid #FFFFFF;
       }
-      
-      
-      
+
+
+
       table td h3{
         color: #0087C3;
         font-size: 1.2em;
         font-weight: normal;
         margin: 0 0 0.2em 0;
       }
-      
+
       table .no {
         color: #FFFFFF;
         background: #0087C3;
       }
-      
+
       table .desc {
         text-align: left;
         width: 3cm;
       }
-      
+
       table .unit {
         background: #DDDDDD;
       }
-      
+
       table .total {
         text-align: right  !important;
       }
-      
+
+
+
+      table .qty {
+      }
+
       table .total {
         background: #0087C3;
         color: #FFFFFF;
       }
-      
+
       table td.unit,
       table td.qty {
         text-align: left;
       }
-      
+
       table tbody tr:last-child td {
         border: none;
       }
-      
+
       table tfoot td {
         padding: 10px 20px;
         background: #FFFFFF;
@@ -172,38 +185,38 @@
         font-size: 1.2em;
         border-top: 1px solid #AAAAAA; 
       }
-      
+
       table tfoot tr:first-child td {
         border-top: none; 
       }
-      
+
       table tfoot tr:last-child td {
         color: #57B223;
         font-size: 1.4em;
         border-top: 1px solid #57B223; 
-      
+
       }
-      
+
       table tfoot tr td:first-child {
         border: none;
       }
-      
+
       .thanks{
         margin-bottom: 50px;
         margin-top: 50px;
         color: #9e9e9e;
       }
-      
+
       #notices{
         padding-left: 6px;
         border-left: 6px solid #0087C3;  
       }
-      
+
       #notices .notice {
         font-size: 1.2em;
       }
-      
-      footer {
+
+      #footer {
         color: #777777;
         width: 100%;
         height: 30px;
@@ -213,29 +226,35 @@
         padding: 8px 0;
         text-align: center;
       }
-      
-  
+
+
     </style>
   </head>
+
   <body>
-    <header class="clearfix">
+
+    <div class="clearfix" id="header">
+
       <div id="logo">
         <img src="{{ url('/storage/app/public/logos/logo2.png') }}">
       </div>
+
       <div id="company">
         <h2 class="name">Guru Kitchen Ltd</h2>
         <div class="address">Unit 7 Cooper Way, Parkhouse, Carlisle, Cumbria, United Kingdom, CA3 0JG</div>
         <div>accounts@gurukitchen.co.uk</div>
-        <div>VAT: XYZABCPQRZ</div>
+        <div>VAT: 373 5643 78</div>
       </div>
-      </div>
-    </header>
-    <main>
+
+    </div>
+
+    <div id="main">
+
       <div id="details" >
         <div id="client">
           <div class="to">INVOICE TO:</div>
           <h2 class="name">Quarter Lounge</h2>
-          <div class="address">Unit 7 Cooper Way, Parkhouse, Carlisle, Cumbria, United Kingdom, CA3 0JG</div>
+          <div class="address">Unit 7 Cooper Way, Parkhouse, Carlisle, Cumbria, United Kingdom, CA3 0JG Unit 7 Cooper Way, Parkhouse, Carlisle, Cumbria, United Kingdom, CA3 0JG</div>
         </div>
         <div id="invoice">
           <h1>TAX INVOICE #3256</h1>
@@ -245,6 +264,7 @@
       </div>
 
       <h2 class="heading">GuruKitchen Commission</h2>
+
       <table cellspacing="0" cellpadding="0">
 
 
@@ -297,9 +317,11 @@
         <div class="notice">Invoice for service rendered: food and beverage marketplace platform and delivery.</div>
       </div>
 
-    </main>
-    <footer>
+    </div>
+
+    <div id="footer">
       Invoice was created on a computer and is valid without the signature and seal.
-    </footer>
+    </div>
+
   </body>
 </html>
