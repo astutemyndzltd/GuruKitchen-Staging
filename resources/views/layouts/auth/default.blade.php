84a53e4be3b787b617b3e4197d4a050eae35e024
Anik Banerjee <anik@astutemyndz.com>
2021-03-31 15:31:53 +0530

    Support for new logo

--- resources/views/layouts/auth/default.blade.php
+++ resources/views/layouts/auth/default.blade.php
@@ -27,7 +27,7 @@
     <div class="login-logo">
         <a href="{{ url('/') }}">
 			{{-- <img src="{{$app_logo}}" alt="{{setting('app_name')}}"> --}}
-			<img src="https://portal.gurukitchen.co.uk/storage/app/public/1/logo-bg.png" alt="{{setting('app_name')}}" style="max-height:100px;max-width:600px;">
+			<img src="{{url('images/logo-bg.png')}}" alt="{{setting('app_name')}}" style="max-height:100px;max-width:600px;">
 		</a>
     </div>
     <!-- /.login-logo -->
