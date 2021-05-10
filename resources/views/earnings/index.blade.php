9979805af4dc30ac396fe5d5baa0221f1a8b107b
Anik Banerjee <anik@astutemyndz.com>
2021-05-10 12:39:22 +0530

    Setting up drivers

--- resources/views/earnings/index.blade.php
+++ resources/views/earnings/index.blade.php
@@ -30,6 +30,9 @@
         <li class="nav-item">
           <a class="nav-link active" href="{!! url()->current() !!}"><i class="fa fa-list mr-2"></i>{{trans('lang.earning_table')}}</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="{!! url()->current() !!}"><i class="fa fa-list mr-2"></i>{{trans('lang.earning_table')}}</a>
+        </li>
         @can('earnings.create')
         <li class="nav-item">
           <a class="nav-link" href="{!! route('earnings.create') !!}"><i class="fa fa-plus mr-2"></i>{{trans('lang.earning_create')}}</a>
