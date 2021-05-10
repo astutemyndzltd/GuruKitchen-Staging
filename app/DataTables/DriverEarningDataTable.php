cf3af9994c8d9da39b5286e29eaeaadb0ae2c62f
Anik Banerjee <anik@astutemyndz.com>
2021-05-10 13:15:18 +0530

    Setting up drivers

---
+++ app/DataTables/DriverEarningDataTable.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\CustomField;
+use App\Models\Earning;
+use App\Models\Order;
+use Barryvdh\DomPDF\Facade as PDF;
+use Yajra\DataTables\CollectionDataTable;
+use Yajra\DataTables\Services\DataTable;
+use Illuminate\Support\Facades\DB;
+
+class DriverEarningDataTable extends DataTable
+{
+    /**
+     * custom fields columns
+     * @var array
+     */
+    public static $customFields = [];
+
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($collection)
+    {        
+        $dataTable = new CollectionDataTable(collect($collection));
+        $columns = array_column($this->getColumns(), 'data');
+        $dataTable = $dataTable
+            ->editColumn('rest_name', function ($result) {
+                return $result->rest_name;
+            })
+            ->editColumn('total', function ($result) {
+                return $result->total;
+            })
+            ->editColumn('gross', function ($result) {
+                return getPrice($result->gross);
+            })
+            ->editColumn('commission_tax', function ($result) {
+                $taxRate = setting('default_tax', 0);
+                $comRate = $result->commission;
+                $commission = round(($comRate / 100) * $result->gross, 2);
+                $taxTotal = round(($taxRate / 100) * $commission, 2);
+                return getPrice($commission) . " / " . getPrice($taxTotal);
+            })
+            ->editColumn('earning', function ($result) {
+                $taxRate = setting('default_tax', 0);
+                $comRate = $result->commission;
+                $commission = round(($comRate / 100) * $result->gross, 2);
+                $taxTotal = round(($taxRate / 100) * $commission, 2);
+                $net = $result->gross - ($commission + $taxTotal);
+                return getPrice($net);
+            })
+            ->editColumn('period', function ($result) {
+                if (isset($result->startdate) && isset($result->enddate)) {
+                    return date('d M Y', strtotime($result->startdate)) . ' - ' . date('d M Y', strtotime($result->enddate)); 
+                }
+                else {
+                    return 'NA';
+                }       
+            })
+            ->rawColumns($columns);
+
+        return $dataTable;
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        $columns = [
+            [
+                'data' => 'rest_name',
+                'title' => 'Restaurant',
+                'orderable' => false
+
+            ],
+            [
+                'data' => 'total',
+                'title' => 'Orders',
+                'orderable' => false,
+                'searchable' => false
+
+            ],
+            [
+                'data' => 'gross',
+                'title' => 'Gross Revenue',
+                'orderable' => false,
+                'searchable' => false
+
+            ],
+            [
+                'data' => 'commission_tax',
+                'title' => 'Commission / Tax',
+                'orderable' => false,
+                'searchable' => false
+
+            ],
+            [
+                'data' => 'earning',
+                'title' => 'Earning',
+                'orderable' => false,
+                'searchable' => false
+            ],
+            [
+                'data' => 'period',
+                'title' => 'Period',
+                'orderable' => false,
+                'searchable' => false
+            ]
+        ];
+
+        return $columns;
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\Models\Post $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query()
+    {
+        if (auth()->user()->hasRole('admin')) 
+        {
+            $statement = "select coalesce(d2.total, 0) total, coalesce(d2.gross, 0) gross, r.id rest_id, 
+            r.name rest_name, r.admin_commission commission, date(d2.mindate) startdate, date(d2.maxdate) enddate
+            from (select count(*) total, sum(price) gross, res_id, min(created_at) mindate, max(created_at) maxdate 
+            from (select o.id id, p.price, ro.res_id, o.created_at from orders o
+            join payments p on o.payment_id = p. id and o.paid_out = 0 and o.active = 1
+            join (select fo.order_id, f.restaurant_id res_id from food_orders fo join foods f on fo.food_id = f.id group by fo.order_id) ro
+            on o.id = ro.order_id) d group by res_id) d2 right join restaurants r on r.id = d2.res_id";
+
+            $results = DB::select(DB::raw($statement));
+
+            return $results;
+        }
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            ->parameters(array_merge(
+                config('datatables-buttons.parameters'), [
+                    'language' => json_decode(
+                        file_get_contents(base_path('resources/lang/' . app()->getLocale() . '/datatable.json')
+                        ), true)
+                ]
+            ));
+    }
+
+    /**
+     * Export PDF using DOMPDF
+     * @return mixed
+     */
+    public function pdf()
+    {
+        $data = $this->getDataForPrint();
+        $pdf = PDF::loadView($this->printPreview, compact('data'));
+        return $pdf->download($this->filename() . '.pdf');
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'earningsdatatable_' . time();
+    }
+}
