fd95e0e2c6be1b0e19031ac6f74e447d3d45f1f4
Anik Banerjee <anik@astutemyndz.com>
2021-05-27 22:16:19 +0530

    Support for STAR printer

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -83,20 +83,6 @@
       <div class="clearfix"></div>
     </div>
     <a href="#" id="print" style="display:none;"></a>
-    <div id="candy" style="display:none;">
-      <html>
-      <head>
-          <link rel="preconnect" href="https://fonts.gstatic.com">
-          <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-          <link rel="stylesheet" href="{{ asset('plugins/font-awesome/css/font-awesome.min.css') }}">
-          <link rel="stylesheet" href="{{ asset('css/all.css') }}">
-          <link rel="stylesheet" href="{{ asset('css/receipt.css') }}">
-      </head>
-      <body>
-          @include('orders.receipt')
-      </body>
-    </html>
-    </div>
   </div>
 </div>
 @endsection
