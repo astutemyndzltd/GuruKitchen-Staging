d9ee43aa28899f437caccd26eee48c3abdae8cec
Anik Banerjee <anik@astutemyndz.com>
2021-06-15 20:01:36 +0530

    Order edit style change

--- resources/views/orders/fields.blade.php
+++ resources/views/orders/fields.blade.php
@@ -56,7 +56,7 @@
     <!-- 'Boolean active Field' -->
     @if(auth()->user()->hasRole('admin'))   
         <div class="form-group row">
-            {!! Form::label('active', 'Uncheck to cancel', ['class' => 'col-6 control-label text-right']) !!}
+            {!! Form::label('active', 'Uncheck to cancel', ['class' => 'col-3 control-label text-right']) !!}
             <div class="checkbox icheck">
                 <label class="col-9 ml-2 form-check-inline">
                     {!! Form::hidden('active', 0) !!}
