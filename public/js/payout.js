950334a5d84440f04d0ae78f80155eb01ba3181c
Anik Banerjee <anik@astutemyndz.com>
2021-06-23 00:32:38 +0530

    Order edit style change

--- public/js/payout.js
+++ public/js/payout.js
@@ -36,6 +36,9 @@ async function onDateRangeChange(start, end) {
     txtDeliveryFee.value = json.delivery_fee;
     txtDriverCommission.value = json.driver_commission;
 
+    console.log(txtDeliveryFee.value);
+    console.log(txtDriverCommission.value);
+
     txtAmount.value = `£${json.amount}`;
 
     $(btnSavePayout).attr('disabled', false);
