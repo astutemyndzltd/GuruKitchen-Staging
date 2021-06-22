42ec097e07e3120c63a3b252f2850fa7301884d5
Anik Banerjee <anik@astutemyndz.com>
2021-06-22 21:32:31 +0530

    Order edit style change

--- app/DataTables/EarningDataTable.php
+++ app/DataTables/EarningDataTable.php
@@ -35,6 +35,9 @@ class EarningDataTable extends DataTable
             ->editColumn('total', function ($result) {
                 return $result->total;
             })
+            ->editColumn('total_delivered', function ($result) {
+                return $result->total_delivered;
+            })
             ->editColumn('gross', function ($result) {
                 return getPrice($result->gross);
             })
@@ -88,6 +91,13 @@ class EarningDataTable extends DataTable
 
             ],
             [
+                'data' => 'total_delivered',
+                'title' => 'Delivered',
+                'orderable' => false,
+                'searchable' => false
+
+            ],
+            [
                 'data' => 'gross',
                 'title' => 'Gross Revenue',
                 'orderable' => false,
@@ -128,10 +138,17 @@ class EarningDataTable extends DataTable
     {
         if (auth()->user()->hasRole('admin')) 
         {
-            $statement = "select coalesce(d2.total, 0) total, coalesce(d2.gross, 0) gross, r.id rest_id, 
-            r.name rest_name, r.admin_commission commission, date(d2.mindate) startdate, date(d2.maxdate) enddate
-            from (select count(*) total, sum(price) gross, res_id, min(created_at) mindate, max(created_at) maxdate 
-            from (select o.id id, p.price, ro.res_id, o.created_at from orders o
+            $driverCommission = setting('driver_commission', 0);
+            
+            $statement = "select coalesce(d2.total, 0) total, coalesce(d2.total_delivered, 0) total_delivered,
+            coalesce(d2.gross, 0) gross, driver_fee, driver_commission, r.id rest_id, r.name rest_name, 
+            r.admin_commission commission, date(d2.mindate) startdate, date(d2.maxdate) enddate
+            from (select count(*) total, count(case when order_status_id = 5 then 1 else null end) total_delivered,
+            sum(if(order_status_id = 5, price, 0)) gross, sum(if(order_status_id = 5, app_del_fee, 0)) driver_fee, 
+            sum(if(order_status_id = 5, driver_com, 0)) driver_commission, res_id, min(created_at) mindate, max(created_at) maxdate 
+            from (select o.id id, p.price, if(o.use_app_drivers = 1, o.delivery_fee, 0) app_del_fee,
+            (if(o.use_app_drivers = 1, $driverCommission, 0) / 100 * (p.price - o.delivery_fee)) driver_com, 
+            o.delivery_fee, ro.res_id, o.created_at, o.order_status_id from orders o
             join payments p on o.payment_id = p. id and o.paid_out = 0 and o.active = 1
             join (select fo.order_id, f.restaurant_id res_id from food_orders fo join foods f on fo.food_id = f.id group by fo.order_id) ro
             on o.id = ro.order_id) d group by res_id) d2 right join restaurants r on r.id = d2.res_id";
